beaced29ed575542b0815f6b7f1799b4716993e7
Scott Madin <smadin@gmail.com>
2020-12-07 16:38:37 -0500

    placeholder for scuttle.sh

---
+++ src/scuttle.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+
+# TODO
+# - check test dir
+# - parse test source files
+# - build suites of test cases
+# - generate headers per suite
+# - generate harness
+# - generate testdir Makefile!
+# - build and run harness
+
+# syntax: bash scuttle.sh [<testdir>]
+# if <testdir> not provided, assume 'test'
